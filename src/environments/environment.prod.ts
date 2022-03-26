bcef3912c55314de8f5a2bca47db03d4904b0a65
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2022-03-26 21:33:43 +0800

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'adrian-firebase',
+    appId: '1:681495070524:web:8b965793a9d3e233c0c039',
+    storageBucket: 'adrian-firebase.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyAIukLvK6-LFmuCtlAw9VMldKCtgGi9oEs',
+    authDomain: 'adrian-firebase.firebaseapp.com',
+    messagingSenderId: '681495070524',
+    measurementId: 'G-GX161ZF745',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'adrian-firebase',
+    appId: '1:681495070524:web:8b965793a9d3e233c0c039',
+    storageBucket: 'adrian-firebase.appspot.com',
+    locationId: 'asia-southeast1',
+    apiKey: 'AIzaSyAIukLvK6-LFmuCtlAw9VMldKCtgGi9oEs',
+    authDomain: 'adrian-firebase.firebaseapp.com',
+    messagingSenderId: '681495070524',
+    measurementId: 'G-GX161ZF745',
+  },
   production: false
 };
 
