ef3e948e656fdc47b9ec3d1d591a830fe13307f2
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2022-05-27 10:32:19 +0800

    firebase

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -7,7 +7,13 @@ import { DashboardComponent } from "./pages/dashboard/dashboard.component";
 const routes: Routes = [
   {
     path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
     component: VerticalLayout1Component,
+    pathMatch: 'full',
     children: [
       {
         path: '',
