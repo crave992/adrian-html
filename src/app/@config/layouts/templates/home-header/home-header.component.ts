1ac6ba1bbf64dc37a6c2965ee1aa760ed30450e3
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2022-05-31 19:05:39 +0800

    navbar

--- src/app/@config/layouts/templates/home-header/home-header.component.ts
+++ src/app/@config/layouts/templates/home-header/home-header.component.ts
@@ -8,22 +8,13 @@ import { Route, Router } from '@angular/router';
   ]
 })
 export class HomeHeaderComponent implements OnInit {
-  routes: any[] = [];
+
   constructor(
-    private _router: Router,
+
   ) { }
 
   ngOnInit(): void {
-    this.printpath('', this._router.config);
+
   }
-  printpath(parent: String, config: Route[]) {
-    for (let i = 0; i < config.length; i++) {
-      const route = config[i];
-      this.routes.push({ path: parent + '/' + route.path, data:route.data });
-      if (route.children) {
-        const currentPath = route.path ? parent + '/' + route.path : parent;
-        this.printpath(currentPath, route.children);
-      }
-    }
-  }
+
 }
