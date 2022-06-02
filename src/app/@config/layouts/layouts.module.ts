88eef37507b8730c3ce7b034f1f94c8dc3ccda05
Adrian Jairus Valbuena <adrianwebpaypal@gmail.com>
2022-06-02 14:51:29 +0800

    banner

--- src/app/@config/layouts/layouts.module.ts
+++ src/app/@config/layouts/layouts.module.ts
@@ -6,13 +6,15 @@ import { HomeHeaderComponent } from './templates/home-header/home-header.compone
 import { FooterComponent } from './templates/footer/footer.component';
 import { VerticalLayout1Component } from './vertical-layout1/vertical-layout1.component';
 import { AdminModule } from 'src/app/admin/admin.module';
+import { BannerComponent } from './templates/banner/banner.component';
 
 @NgModule({
   declarations: [
     HorizontalComponent,
     HomeHeaderComponent,
     FooterComponent,
-    VerticalLayout1Component
+    VerticalLayout1Component,
+    BannerComponent
   ],
   imports: [
     CommonModule,
